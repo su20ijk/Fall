c1ac821a0ffa6234f4ea5b38c8c656f281f443d3
su20 <19liuq@elmbrookstudents.org>
2018-06-18 09:05:39 -0500

    initial project

---
+++ Fall/Fall/Fall.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fall
+{
+    public class Fall
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Brookfield Central High School");
+            Console.ReadKey();
+        }
+    }
+}
